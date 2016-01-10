416122e5bc427e0d0445a73f95520be41c0f173a
весела <adresza@abv.bg>
2016-01-11 00:08:40 +0200

    Създаване на директория

--- IOTesting/IOTextfiles/IOSettings.cs
+++ IOTesting/IOTextfiles/IOSettings.cs
@@ -13,12 +13,15 @@ namespace IOTextfiles
 
 		public string getPath ()
 		{
-			string _path = System.IO.Path.Combine (AppDomain.CurrentDomain.BaseDirectory, "test1.txt");
+			//Програма\Settings\settings.txt
+
+			string _path = System.IO.Path.Combine (AppDomain.CurrentDomain.BaseDirectory, "Settings");
+			_path = System.IO.Path.Combine(_path, "Settings.txt");
 
 			//други видове директории
 
-			string _user = Environment.GetFolderPath (Environment.SpecialFolder.LocalApplicationData); Console.WriteLine ( _user);
-			string _desktop = Environment.GetFolderPath (Environment.SpecialFolder.Desktop);            Console.WriteLine ( _desktop);
+			//string _user = Environment.GetFolderPath (Environment.SpecialFolder.LocalApplicationData); Console.WriteLine ( _user);
+			//string _desktop = Environment.GetFolderPath (Environment.SpecialFolder.Desktop);            Console.WriteLine ( _desktop);
 
 			return _path;
 		}
@@ -44,7 +47,8 @@ namespace IOTextfiles
 		public bool open ()
 		{
 			try {
-				
+
+				iniSettings();
 				string _temp = "", _filePath = getPath();
 
 				if (System.IO.File.Exists(_filePath))  //проверка дали пътят е валиден
@@ -60,7 +64,7 @@ namespace IOTextfiles
 					}
 
 					}else{
-						Console.WriteLine("Не е намерен такъв път");
+					
 
 						return false;
 						}
@@ -73,6 +77,30 @@ namespace IOTextfiles
 			}
 
 			return false;
+		}
+
+		private void iniSettings()
+		{
+			try{
+
+				bool _fileExist = System.IO.File.Exists(getPath());
+
+				if( ! _fileExist)
+				{
+					string _directory = System.IO.Path.GetDirectoryName(getPath());
+
+					//застраховаме се, че директорията съществува
+					if (!System.IO.Directory.Exists (_directory))
+					{
+						System.IO.Directory.CreateDirectory (_directory);
+					}
+
+					//Запаметяваме съдържанието на файла
+					save();
+				}
+
+			}catch{
+			}
 		}
 	}
 }
