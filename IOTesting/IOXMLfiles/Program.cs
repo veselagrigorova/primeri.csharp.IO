b509ce0e6d1e747ff1d6886c29e5eb9f1d5f7ad9
весела <adresza@abv.bg>
2016-01-16 12:48:28 +0200

    Въведение в XML

---
+++ IOTesting/IOXMLfiles/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace IOXMLfiles
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			string value = "simple value", property = "simple property";
+
+			string path = System.IO.Path.Combine (AppDomain.CurrentDomain.BaseDirectory, "test.xml");
+			using (XmlWriter writer = XmlWriter.Create (path)) {
+				writer.WriteStartDocument ();
+
+				writer.WriteStartElement ("settings");
+
+				//съдържание на файла
+
+				writer.WriteStartElement ("row");
+
+				writer.WriteAttributeString ("property", property );
+				writer.WriteString ( value );
+
+				writer.WriteEndElement ();
+				writer.WriteEndElement ();
+				writer.WriteEndDocument ();
+			}
+
+			XDocument document = XDocument.Load (path);
+			document.Save (path);
+
+			System.Diagnostics.Process.Start(path);
+		}
+	}
+}
